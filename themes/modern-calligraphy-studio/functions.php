b55c05033861ca12886f7f7d9b0eee5e7d47ce53
Maximilian Coghlan <max.coghlan@icloud.com>
2020-11-14 13:03:08 +0000

    laravel mix setup

--- themes/modern-calligraphy-studio/functions.php
+++ themes/modern-calligraphy-studio/functions.php
@@ -3,6 +3,9 @@
   function load_stylesheets() {
     wp_register_style('stylesheet', get_template_directory_uri() . '/style.css', '', 1, 'all');
     wp_enqueue_style('stylesheet');
+
+    wp_register_style('custom', get_template_directory_uri() . '/app.css', '', 1, 'all');
+    wp_enqueue_style('custom');
   }
 
   add_action('wp_enqueue_scripts', 'load_stylesheets');
